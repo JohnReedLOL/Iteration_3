3e8ea079825b3f3939866aea68aeca1ca82a359e
John-Michael Reed <JohnReedLOL@ufl.edu>
2015-03-29 18:31:28 -0400

    Added GUI

---
+++ Outermost_Package/View/Game_Screen_JPanel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author JohnReedLOL
+ */
+public class Game_Screen_JPanel extends JPanel {
+    public Game_Screen_JPanel() {
+        super();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);       
+
+        // Draw Text
+        g.drawString("This is my custom Game_Screen_JPanel!",10,20);
+    }  
+}
---
+++ Outermost_Package/View/Minimap_JTextPanel.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package View;
+
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author JohnReedLOL
+ */
+public class Minimap_JTextPanel extends JPanel {
+    public Minimap_JTextPanel() {
+        super();
+    }
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);       
+
+        // Draw Text
+        g.drawString("This is my custom Minimap_JTextPanel!",10,20);
+    }  
+}
