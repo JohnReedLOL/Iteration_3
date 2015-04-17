77ddc1f07c454714be5ed7499ca9f9e23fc1fe08
wyeung <wyeung@ufl.edu>
2015-04-16 20:28:15 -0400

    Added basic viewports and screens

---
+++ src/mvc_bridgeway/screen/PauseScreen.java
@@ -0,0 +1,58 @@
+package mvc_bridgeway.screen;
+
+// @author comcc_000
+
+import controller.physicalController.PhysicalController;
+import controller.virtual_controller.VirtualController;
+import java.util.ArrayList;
+import model.Model;
+import model.UserSettings;
+import mvc_bridgeway.control_map.ControlMap;
+import view.viewport.PauseViewport;
+import view.viewport.Viewport;
+
+
+public class PauseScreen extends Screen {
+
+    /*Properties*/
+
+    /*Constructors*/
+    
+    public PauseScreen() {
+        super();
+    }
+
+    /*Methods*/
+    
+    @Override
+    protected Viewport createView() {
+        return new PauseViewport();
+    }
+
+    @Override
+    protected PhysicalController createPhysicalController(Model model, PhysicalController physicalController, UserSettings userSettings) {
+        return physicalController;
+    }
+
+    @Override
+    protected ArrayList<ControlMap> getUserControls(UserSettings userSettings) {
+        return null;
+    }
+
+    @Override
+    protected ArrayList<ControlMap> generateDefaultPhysicalControlMaps() {
+        return new ArrayList<ControlMap>();
+    }
+
+    @Override
+    protected void setUserControls(UserSettings userSettings, ArrayList<ControlMap> defaultControls) {
+        //TODO: throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /*Get-Sets*/
+
+    /*Inner-classes*/
+
+    /*Test Main Method*/
+
+}
