ab64369a4cb6f761cd3e704f2321e696ab5c26e2
Matthew Kroeze <matthew.kroeze@gmail.com>
2015-04-17 13:10:48 -0400

    ability system almost comlete

---
+++ src/model/entity/ability/AbilityLibrary.java
@@ -0,0 +1,57 @@
+package model.entity.ability;
+
+import java.util.List;
+
+import model.entity.Entity;
+
+public class AbilityLibrary {
+	private List<Ability> learnedAbilities;
+	private List<Ability> unlearnedAbilities;
+	private Entity owner;
+	
+	public AbilityLibrary(Entity owner){
+		this.owner = owner;
+	}
+	
+	public void insertAbility(Ability ability){
+		if(ability.meetsLearnPrerequisites(owner)){
+			if(!learnedAbilities.contains(ability)) learnedAbilities.add(ability);
+		}
+		else{
+			if(!unlearnedAbilities.contains(ability)) unlearnedAbilities.add(ability);
+		}
+	}
+	
+	public void removeAbility(Ability ability){
+		if(ability.meetsLearnPrerequisites(owner)){
+			if(learnedAbilities.contains(ability)) learnedAbilities.remove(ability);
+		}
+		else{
+			if(unlearnedAbilities.contains(ability)) unlearnedAbilities.remove(ability);
+		}
+	}
+	
+	public boolean useAbility(int position){
+		if(position < learnedAbilities.size() && learnedAbilities.get(position).meetsUsePrerequisites(owner)){
+			learnedAbilities.get(position).performEffect(owner);
+			return true;
+		}
+		return false;
+		
+	}
+	
+	public void notifyOfOwnerChanges(){
+		for(Ability a : learnedAbilities){
+			if(!a.meetsLearnPrerequisites(owner)){
+				unlearnedAbilities.add(a);
+				learnedAbilities.remove(a);
+			}
+		}
+		for(Ability a : unlearnedAbilities){
+			if(a.meetsLearnPrerequisites(owner)){
+				learnedAbilities.add(a);
+				unlearnedAbilities.remove(a);
+			}
+		}
+	}
+}
