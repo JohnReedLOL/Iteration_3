09a7802fa8b119e7722a93c3cbac4ecfa74b425d
John Reed <JohnReedLOL@ufl.edu>
2015-04-18 14:22:48 -0400

    renderable thing implemented

--- src/view/viewport/RenderableThing.java
+++ src/view/viewport/RenderableThing.java
@@ -9,23 +9,34 @@ import application.Application;
 import java.awt.image.BufferedImage;
 import javax.swing.JLabel;
 import model.MapObject;
+import src.view.Renderable;
 import view.viewport.screen_viewport.GameScreenViewport;
 
 /**
  *
  * @author johnmichaelreed2
  */
-public class RenderableThing<T extends MapObject> {
-    final MapObject to_render_;
+public class RenderableThing<T extends MapObject> implements Renderable {
+    final T to_render_;
     public RenderableThing(T thing) {
         to_render_ = thing;
+    }
+    
+    @Override
+    public int renderAsImageToJPanel(GameViewport game_viewport, int x, int y) {
+        return 0;
+    }
+    
+    @Override
+    public int renderAsDotToJPanel(MiniMapViewport game_viewport, int x, int y) {
+        return 0;
     }
     /**
      * Renders this wrapper object to a JLabel
      * @param panel - The viewport to render myself to
      * @return 0 on success, -1 on failure
      */
-    public int renderAsImageOnjPanel(GameScreenViewport panel) {
+    public int renderAsImageOnJLabel(JLabel label) {
         return 0;
     }
     
@@ -36,6 +47,9 @@ public class RenderableThing<T extends MapObject> {
     }
     
     public BufferedImage getMyImageRepresentation() {
+        
+        final String class_name;
+        
         Application.check(false, "not yet implemented");
         return null;
     }
