d084e2284e2b8eb0382d517139c2d4ed1a33b1c0
tgittelmacher <tgittelmacher@ufl.edu>
2015-04-18 18:44:18 -0400

    Amazing bi-directional map
    For the glory of Satan

--- src/model/map/BidirectionalMap.java
+++ src/model/map/BidirectionalMap.java
@@ -1,10 +1,78 @@
 package model.map;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by Troy on 4/18/2015.
  */
-public class BidirectionalMap {
+public class BidirectionalMap<K, V> {
 
-   // private Map<K, V> map;
+    private Map<K, V> keyToValueMap;
+    private Map<V, K> valueToKeyMap;
 
+    public BidirectionalMap() {
+        keyToValueMap = new HashMap<K, V>();
+        valueToKeyMap = new HashMap<V, K>();
+    }
+
+    public void insert( K key, V value ) {
+        keyToValueMap.put( key, value );
+        valueToKeyMap.put( value, key );
+    }
+
+    public void clear() {
+        keyToValueMap.clear();
+        valueToKeyMap.clear();
+    }
+
+    public boolean containsKey( K key  ) {
+        return keyToValueMap.containsKey( key );
+    }
+
+    public boolean containsValue( V value ) {
+        return keyToValueMap.containsValue(value);
+    }
+
+    public V getValue( K key ) {
+        return keyToValueMap.get(key);
+    }
+
+    public K getKey( V value ) {
+        return valueToKeyMap.get(value);
+    }
+
+    public boolean isEmpty() {
+        return keyToValueMap.isEmpty();
+    }
+
+    public void removeKey( K key ) {
+        V v = keyToValueMap.get( key );
+        keyToValueMap.remove( key );
+        valueToKeyMap.remove( v );
+    }
+
+    public void removeValue( V value ) {
+        K k = valueToKeyMap.get( value );
+        keyToValueMap.remove( value );
+        valueToKeyMap.remove( k );
+    }
+
+    public void replace( K key, V value ) {
+        keyToValueMap.replace(key, value);
+        valueToKeyMap.replace(value, key);
+    }
+
+    public int size() {
+        return keyToValueMap.size();
+    }
+
+    public Collection<K> getKeyValues() {
+        return valueToKeyMap.values();
+    }
+
+    public Collection<V> getValueValues() {
+        return keyToValueMap.values();
+    }
 }
