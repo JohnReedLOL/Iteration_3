111b0474160a8226aa5d21576c16eb08b2a75107
tgittelmacher <tgittelmacher@ufl.edu>
2015-04-19 19:38:11 -0400

    Map teleporting works
    You can now move a MapObject to another DiscreteMap.

--- src/test/MapTest.java
+++ src/test/MapTest.java
@@ -4,10 +4,13 @@ import model.armory.Armory;
 import model.entity.Entity;
 import model.entity.avatar.Avatar;
 import model.entity.behavior.movement.ClassicMovementBehavior;
+import model.entity.npc.pet.Pet;
 import model.entity.stats.StatsOwnership;
 import model.inventory.Sack;
 import model.map.DiscreteMap;
 import model.map.GameMap;
+import model.map.SecondLevelGameMap;
+import model.map.coordinate.Coordinate2D;
 import model.map.coordinate.HexCoordinate;
 import model.map.location.Location;
 import model.map.location.Tile;
@@ -19,15 +22,31 @@ public class MapTest {
 
     public static void main(String[] args) {
         DiscreteMap map = new GameMap();
+        map.populate();
+
+        DiscreteMap otherMap = new SecondLevelGameMap();
+        otherMap.populate();
 
         HexCoordinate coord = new HexCoordinate(1, 7);
-        //Entity entity = new Avatar("AVATAR", "TROY", new Armory(), new Sack(), ClassicMovementBehavior.getInstance(), new StatsOwnership(null));
+        Entity entity = new Avatar( coord );
 
         //map.insert( entity, map.getLocationByCoordinate(coord) );
 
-        Tile tile = (Tile) map.getLocationByCoordinate( coord );
+        map.insert(entity, map.getLocationByCoordinate(coord));
 
-        coord = (HexCoordinate) map.getCoordinateByLocation( tile );
+        System.out.println(map.getLocationByMapObject(entity));
+
+        System.out.println(map.getLocationByCoordinate(new HexCoordinate(1, 7)).getMapObjects().contains(entity));
+
+        map.move(entity, map.getCoordinateByLocation(map.getLocationByMapObject(entity)), new HexCoordinate(5, 5));
+
+        System.out.println(map.getLocationByCoordinate(new HexCoordinate(5, 5)).getMapObjects().contains(entity));
+
+        map.teleport(entity, otherMap);
+        otherMap.move(entity, otherMap.getCoordinateByLocation(otherMap.getLocationByMapObject(entity)), new HexCoordinate(5, 6));
+
+        System.out.println(" FIRST MAP : " +map.getLocationByCoordinate(new HexCoordinate(5, 6)).getMapObjects().contains(entity));
+        System.out.println( "SECOND MAP : " +otherMap.getLocationByCoordinate(new HexCoordinate(5, 5)).getMapObjects().contains(entity) );
 
         int poop = 69;
 
