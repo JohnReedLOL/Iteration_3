eb6e0d564cb24f1a80919b3522869ac6e61650e5
tgittelmacher <tgittelmacher@ufl.edu>
2015-04-19 20:42:26 -0400

    Game Strings
    view should use these?

--- src/view/utility/GameStrings.java
+++ src/view/utility/GameStrings.java
@@ -6,6 +6,86 @@ package view.utility;
 public abstract class GameStrings {
 
     //PUT ALL YER STRING HERE!
+
+    /**
+     * MAIN MENU OPTIONS
+     */
+
+    public static final String MAIN_MENU_LOAD = "Load";
+    public static final String MAIN_MENU_NEW_GAME = "New Game";
+    public static final String MAIN_MENU_EXIT = "Exit";
+
+    /**
+     * OCCUPATIONS
+     */
+
     public static final String OCCUPATION_SMASHER = "Smasher";
+    public static final String OCCUPATION_SNEAK = "Sneak";
+    public static final String OCCUPATION_SUMMONER = "Summoner";
+
+    /**
+     * TABS
+     */
+
+    public static final String TABS_STATS = "Stats";
+    public static final String TABS_SKILLS = "Skills";
+    public static final String TABS_INVENTORY = "Inventory";
+    public static final String TABS_ARMORY = "Armory";
+
+    /**
+     * STATS
+     */
+
+    public static final String STATS_SKILL_POINTS = "Skill points";
+    public static final String STATS_STATS_POINTS = "Stats points";
+    public static final String STATS_CURRENCY = "Booty";
+    public static final String STATS_AFFINITY = "Affinity";
+    public static final String STATS_AGILITY = "Agility";
+    public static final String STATS_ARMOR = "Armor";
+    public static final String STATS_ARMOR_MODIFIER = "Armor Modifier";
+    public static final String STATS_BARGAIN = "Bargain";
+    public static final String STATS_BIND_WOUNDS = "Bind wounds";
+    public static final String STATS_CURRENT_LIFE = "Current Life";
+    public static final String STATS_DEFENSE = "Defense";
+    public static final String STATS_EXPERIENCE = "Experience";
+    public static final String STATS_INTELLECT = "Intellect";
+    public static final String STATS_LEVEL = "Level";
+    public static final String STATS_MAX_LIFE = "Max Life";
+    public static final String STATS_MAX_MANA = "Max Mana";
+    public static final String STATS_MOVEMENT = "Movement";
+    public static final String STATS_OBSERVATION = "Observation";
+    public static final String STATS_OFFENSE = "Offense";
+    public static final String STATS_STRENGTH = "Strength";
+    public static final String STATS_WEAPON_MODIFIER = "Weapon Modifier";
+    public static final String STATS_END_OF_STATS = "End of stats";
+
+    /**
+     * SKILLS
+     */
+
+    public static final String SKILLS_SKILL = "Skill";
+
+    /**
+     * INVENTORY
+     */
+
+    public static final String INVENTORY_ITEM = "Item";
+
+    /**
+     * ARMORY
+     */
+
+    public static final String ARMORY_HEAD = "Head";
+    public static final String ARMORY_BODY = "Body";
+    public static final String ARMORY_LEFT = "Left";
+    public static final String ARMORY_RIGHT = "Right";
+    public static final String ARMORY_LEGS = "Legs";
+
+
+
+
+
+
+
 
 }
